e184140593af652b2f3022f4d10972f2aefe075b
Ricardo J. Mendez <ricardo@arges-systems.com>
2014-07-21 20:19:14 +0300

    Converted LibNoise tutorial #4
    See http://libnoise.sourceforge.net/tutorials/tutorial4.html for the
    original explanation.

---
+++ Assets/Tutorials/Tutorial4.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using LibNoise.Unity;
+using LibNoise.Unity.Generator;
+
+/// <summary>
+/// See http://libnoise.sourceforge.net/tutorials/tutorial4.html for an explanation
+/// </summary>
+public class Tutorial4 : MonoBehaviour 
+{
+	[SerializeField] Gradient _gradient = GradientPresets.Terrain;
+
+	[SerializeField] float _left = 6;
+
+	[SerializeField] float _right = 10;
+
+	[SerializeField] float _top = 1;
+
+	[SerializeField] float _bottom = 5;
+
+	[SerializeField] int _octaveCount = 1;
+
+	[SerializeField] float _frecuency = 1;
+
+	[SerializeField] float _persistence = 0.5f;
+
+	void Start()
+	{
+		var perlin = new Perlin();
+		perlin.OctaveCount = _octaveCount;
+		perlin.Frequency = _frecuency;
+		perlin.Persistence = _persistence;
+		// Unlike on the base LibNoise tutorial, we don't have a separate heightMap target
+		// to set - we will instead build it after.  We also initialize the resulting size
+		// on the constructor instead of passing a separate destination size.
+		var heightMapBuilder = new Noise2D(256, 256, perlin);
+		heightMapBuilder.GeneratePlanar(_left, _right, _top, _bottom);
+
+		// Get the image
+		var image = heightMapBuilder.GetTexture(_gradient);
+
+		// Set it. It may appear inverted from the example on the LibNoise site depending 
+		// on the angle at which the object is rotated/viewed.
+		renderer.material.mainTexture = image;
+
+		// We don't do the light changes for the texture, since that's beyond the scope of 
+		// this port
+	}
+}
