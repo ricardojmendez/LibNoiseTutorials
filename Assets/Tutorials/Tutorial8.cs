d70ccbcdfc0798b8868b6beea0053a62a26e2bea
Ricardo J. Mendez <ricardo@arges-systems.com>
2014-07-25 09:27:38 +0300

    Converted LibNoise tutorial #8
    See http://libnoise.sourceforge.net/tutorials/tutorial8.html for the
    original explanation.
    
    I want to add a normal map as well, but there's a bug I need to look
    into first.

---
+++ Assets/Tutorials/Tutorial8.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using LibNoise;
+using LibNoise.Generator;
+using LibNoise.Operator;
+
+/// <summary>
+/// See http://libnoise.sourceforge.net/tutorials/tutorial8.html for an explanation.
+/// </summary>
+public class Tutorial8 : MonoBehaviour 
+{
+	[SerializeField] Gradient _gradient;
+	[SerializeField] float _west = -180;
+	[SerializeField] float _east = 180;
+	[SerializeField] float _north = -90;
+	[SerializeField] float _south = 90;
+
+	void Start() 
+	{
+		var perlin = new Perlin();
+
+		var heightMapBuilder = new Noise2D(512, 256, perlin);
+		heightMapBuilder.GenerateSpherical(_north, _south, _west, _east);
+
+		var image = heightMapBuilder.GetTexture(_gradient);
+		renderer.material.mainTexture = image;
+
+
+
+	}
+
+}
